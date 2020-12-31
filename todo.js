c893e822b8591049c1d8b4fc7be7231ea61690bc
Mohansingh Omprakash <mohan.singh1385664@gmail.com>
2020-12-31 09:15:16 +0530

    Finished finally

--- todo.js
+++ todo.js
@@ -4,22 +4,22 @@ import os from "os";
 const command = process.argv[2];
 const todo = process.argv[3];
 const filePath = "todo.txt";
-const doneFilePath = "done.txt";
 const deleteMessage = `Deleted todo #${todo}`;
-const doneMessage = `Marked #${todo} as Done`;
+const doneMessage = `Marked todo #${todo} as done.`;
+let usage = `Usage :-
+$ ./todo add "todo item"  # Add a new todo
+$ ./todo ls               # Show remaining todos
+$ ./todo del NUMBER       # Delete a todo
+$ ./todo done NUMBER      # Complete a todo
+$ ./todo help             # Show usage
+$ ./todo report           # Statistics`;
 
 if (!command || command === "help") {
   help();
 }
 
 function help() {
-  return process.stdout.write(`Usage :-
-  $ ./todo add "todo item"  # Add a new todo
-  $ ./todo ls               # Show remaining todos
-  $ ./todo del NUMBER       # Delete a todo
-  $ ./todo done NUMBER      # Complete a todo
-  $ ./todo help             # Show usage
-  $ ./todo report           # Statistics`);
+  return process.stdout.write(usage);
 }
 
 function setTime() {
@@ -66,38 +66,48 @@ function saveTodo(array, path = filePath) {
 }
 
 function addTodo(todo) {
+  if (!todo) {
+    return process.stdout.write(`Error: Missing todo string. Nothing added!`);
+  }
+
   const _ = fs.readFile(filePath, "utf-8", (error, _) => {
     if (error) {
       fs.writeFileSync("todo.txt", todo);
-      return process.stdout.write(`Added Todo: ${todo}`);
+      return process.stdout.write(`Added todo: "${todo}"`);
     } else {
       const tasks = fs.readFileSync("todo.txt", "utf-8");
       let todosArray = tasks.split(os.EOL).filter((value) => value.trim());
       todosArray.push(todo);
       saveTodo(todosArray, filePath);
-      return process.stdout.write(`Added todo: ${todo}`);
+      return process.stdout.write(`Added todo: "${todo}"`);
     }
   });
 }
 
 function ls() {
-  const todos = fs
-    .readFileSync(filePath, "utf-8")
-    .split(os.EOL)
-    .filter((todo) => todo !== "");
-
-  let index = todos.length;
-  for (let i = todos.length; i >= 0; i--) {
-    if (todos[i] === undefined) {
-      continue;
+  const _ = fs.readFile(filePath, "utf-8", (error, data) => {
+    if (error) {
+      return process.stdout.write(`There are no pending todos!`);
     } else {
-      process.stdout.write(`[${index}] ${todos[i]}` + os.EOL);
-      index--;
+      const lists = data.split(os.EOL).filter((task) => task.trim());
+      let index = lists.length;
+      for (let i = lists.length; i >= 0; i--) {
+        if (lists[i] === undefined) {
+          continue;
+        } else {
+          process.stdout.write(`[${index}] ${lists[i]}\n`);
+          index--;
+        }
+      }
     }
-  }
+  });
 }
 
 function deleteTodo(todo, printMessage = deleteMessage) {
+  if (!todo) {
+    return process.stdout.write("Error: Missing NUMBER for deleting todo.");
+  }
+
   let index = parseInt(todo);
   const todos = fs
     .readFileSync(filePath, "utf-8")
@@ -116,6 +126,11 @@ function deleteTodo(todo, printMessage = deleteMessage) {
 }
 
 function doneTodo(todo) {
+  if (!todo) {
+    return process.stdout.write(
+      `"Error: Missing NUMBER for marking todo as done."`
+    );
+  }
   let doneIndex = parseInt(todo);
   const todos = fs
     .readFileSync(filePath, "utf-8")
